1afbc051b2d047062a0aceba2889582beffc5dc1
Леван Попиашвили <teokrin@MacBook-Levan.local>
2020-03-26 17:43:18 +0300

    fix linter errors

--- src/games/game-calc.js
+++ src/games/game-calc.js
@@ -7,14 +7,14 @@ const operations = ['+', '-', '*'];
 
 const calc = (operand1, operand2, operator) => {
   switch (operator) {
-  case '+':
-    return operand1 + operand2;
- case '-':
-    return operand1 - operand2;
-  case '*':
-    return operand1 * operand2;
-  default:
-    return false;
+    case '+':
+      return operand1 + operand2;
+    case '-':
+      return operand1 - operand2;
+    case '*':
+      return operand1 * operand2;
+    default:
+      return false;
   }
 };
 
