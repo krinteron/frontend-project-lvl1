141964f446c83adccaddd9e4e7b8eabd319f5d2e
Леван Попиашвили <teokrin@MacBook-Levan.local>
2020-03-27 17:10:41 +0300

    fix game-prime

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -4,16 +4,16 @@ import getRandom from '../utils.js';
 const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-  if (num % 2 === 0 || num < 3) {
+  if (num < 2) {
     return false;
   }
 
-  const limit = Math.floor(Math.sqrt(num));
-  for (let i = 2; i <= limit; i += 1) {
+  for (let i = 2; i <= num / 2; i += 1) {
     if (num % i === 0) {
       return false;
     }
   }
+
   return true;
 };
 
